2d1257e04519c27e77a52d8dc55f3cf6b66e6659
CodingAgent <crackhopper@gmail.com>
2026-05-08 15:26:16 +0800

    Add Vulkan implicit layer debug toggles

--- src/core/utils/env.cpp
+++ src/core/utils/env.cpp
@@ -6,6 +6,20 @@
 #include <stdlib.h>
 #endif
 
+namespace {
+#ifdef _WIN32
+void setLoaderEnv(const wchar_t *name, const wchar_t *value) {
+  SetEnvironmentVariableW(name, value);
+}
+
+void clearLoaderEnv(const wchar_t *name) { SetEnvironmentVariableW(name, nullptr); }
+#else
+void setLoaderEnv(const char *name, const char *value) { setenv(name, value, 1); }
+
+void clearLoaderEnv(const char *name) { unsetenv(name); }
+#endif
+} // namespace
+
 bool expEnvEnabled(const char *name) {
   const char *value = std::getenv(name);
   return value != nullptr && std::strcmp(value, "0") != 0;
@@ -18,18 +32,49 @@ bool expSceneViewerDebugEnabled() {
 }
 
 void expSetEnvVK() {
+  if (expEnvEnabled("LX_ALLOW_IMPLICIT_VK_LAYERS")) {
+    clearLoaderEnv(
+#ifdef _WIN32
+        L"VK_LOADER_LAYERS_DISABLE"
+#else
+        "VK_LOADER_LAYERS_DISABLE"
+#endif
+    );
+    clearLoaderEnv(
+#ifdef _WIN32
+        L"VK_LOADER_LAYERS_ALLOW"
+#else
+        "VK_LOADER_LAYERS_ALLOW"
+#endif
+    );
+    return;
+  }
+
+  const bool allowRenderDocImplicitLayer =
+      expEnvEnabled("LX_ALLOW_RENDERDOC_IMPLICIT_LAYER");
+
 #ifdef _WIN32
   // SetEnvironmentVariableW(
   //     L"VK_DRIVER_FILES",
   //     LR"(C:\WINDOWS\System32\DriverStore\FileRepository\nvmi.inf_amd64_c6ae241e95feb82d\nv-vk64.json)");
 
-  SetEnvironmentVariableW(L"VK_LOADER_LAYERS_DISABLE", L"~implicit~");
+  setLoaderEnv(L"VK_LOADER_LAYERS_DISABLE", L"~implicit~");
+  if (allowRenderDocImplicitLayer) {
+    setLoaderEnv(L"VK_LOADER_LAYERS_ALLOW", L"VK_LAYER_RENDERDOC_Capture");
+  } else {
+    clearLoaderEnv(L"VK_LOADER_LAYERS_ALLOW");
+  }
 #else
   // setenv(
   //     "VK_DRIVER_FILES",
   //     R"(C:\WINDOWS\System32\DriverStore\FileRepository\nvmi.inf_amd64_c6ae241e95feb82d\nv-vk64.json)",
   //     1);
-  setenv("VK_LOADER_LAYERS_DISABLE", "~implicit~", 1);
+  setLoaderEnv("VK_LOADER_LAYERS_DISABLE", "~implicit~");
+  if (allowRenderDocImplicitLayer) {
+    setLoaderEnv("VK_LOADER_LAYERS_ALLOW", "VK_LAYER_RENDERDOC_Capture");
+  } else {
+    clearLoaderEnv("VK_LOADER_LAYERS_ALLOW");
+  }
 #endif
 }
 
